088d6ed2044dd26a771a7726713e43916029a201
nejarino <njriano2@misena.edu.co>
2024-11-21 14:24:21 -0500

    v0.10_vistaCarritoDeComprasMetodosAñadirQuitarVacio_21112024_nejarino

--- src/main/java/com/sena/ecommerce/controller/HomeUserController.java
+++ src/main/java/com/sena/ecommerce/controller/HomeUserController.java
@@ -1,5 +1,8 @@
 package com.sena.ecommerce.controller;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -9,8 +12,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.sena.ecommerce.model.DetalleOrden;
+import com.sena.ecommerce.model.Orden;
 import com.sena.ecommerce.model.Producto;
 import com.sena.ecommerce.service.IProductoService;
 
@@ -24,6 +31,13 @@ public class HomeUserController {
 	// instancia de objeto - servicio
 	@Autowired
 	private IProductoService productoService;
+
+	// dos variables
+	// lista de detalles de la orden para guardarlos en la db
+	List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+
+	// objeto que almacena los datos de la orden
+	Orden orden = new Orden();
 
 	// metodo que mapea la vista de usuario en la raiz del proyecto
 	@GetMapping("")
@@ -45,6 +59,75 @@ public class HomeUserController {
 		// enviar a la vista con el model los detalles del producto con el id
 		model.addAttribute("producto", p);
 		return "usuario/productoHome";
+	}
+
+	// metodo para enviar del boton de productohome al carrito
+	@PostMapping("/cart")
+	public String addCart(@RequestParam Integer id, @RequestParam Double cantidad, Model model) {
+		DetalleOrden detaorden = new DetalleOrden();
+		Producto p = new Producto();
+		// variable que siempre que este en el metodo inicializa en cero despues de cada
+		// compra
+		double sumaTotal = 0;
+		Optional<Producto> op = productoService.get(id);
+		LOGGER.info("Producto añadido: {}", op.get());
+		LOGGER.info("Cantidad añadida: {}", cantidad);
+		p = op.get();
+		detaorden.setCantidad(cantidad);
+		detaorden.setPrecio(p.getPrecio());
+		detaorden.setNombre(p.getNombre());
+		detaorden.setTotal(p.getPrecio() * cantidad);
+		detaorden.setProducto(p);
+		// validacion para evitar duplicados de productos
+		Integer idProducto = p.getId();
+		// funcion lamda stream y funcion anonima con predicado anyMatch
+		//
+		boolean insertado = detalles.stream().anyMatch(prod -> prod.getProducto().getId() == idProducto);
+		// si no es true añade el producto
+		if (!insertado) {
+			// detalles
+			detalles.add(detaorden);
+		}
+
+		// suma de totales de la lista que el usuario añada al carrito
+		// funcion de java 8 lamda stream
+		// funcion de java 8 anonima dt
+		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+		// pasar variables a la vista
+		orden.setTotal(sumaTotal);
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		return "usuario/carrito";
+	}
+
+	// METODO PARA QUITAR PRODUCTOS DEL CARRITO
+	@GetMapping("/delete/cart/{id}")
+	public String deleteProductoCart(@PathVariable Integer id, Model model) {
+		// lista nueva de productos
+		List<DetalleOrden> ordenesNuevas = new ArrayList<DetalleOrden>();
+		// quitar objeto de la lista de detalleOrden
+		for (DetalleOrden detalleOrden : detalles) {
+			if (detalleOrden.getProducto().getId() != id) {
+				ordenesNuevas.add(detalleOrden);
+			}
+		}
+		// poner la nueva lista con los productos restantes del carrito
+		detalles = ordenesNuevas;
+		// recalcular los productos del carrito
+		double sumaTotal = 0;
+		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+		// pasar variables a la vista
+		orden.setTotal(sumaTotal);
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		return "usuario/carrito";
+	}
+	
+	// metodo para redirigir al carito sin productos
+	public String getCart(Model model) {
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		return "/usuario/carrito";
 	}
 
 }
