ca33914f795070729f0a4bd52af0fafee02398c2
NEJARINO <nestorriano@gmail.com>
2025-02-17 14:27:15 -0500

    v0.16_vistaDetalleComprasdeUsuario&metodoDetalleComprasUsuario_17022025_

--- src/main/java/com/sena/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/sena/ecommerce/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,7 +21,6 @@ import com.sena.ecommerce.service.IUsuarioService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
@@ -29,7 +29,7 @@ public class UsuarioController {
 
 	@Autowired
 	private IUsuarioService usuarioService;
-	
+
 	@Autowired
 	private IOrdenService ordenService;
 
@@ -68,14 +68,14 @@ public class UsuarioController {
 		}
 		return "redirect:/";
 	}
-	
+
 	@GetMapping("/cerrar")
 	public String cerrarSesion(HttpSession session) {
 		session.removeAttribute("idUsuario");
 		return "redirect:/";
 	}
-	
-	//metodo para redirigir a la vista de compras de l usuario
+
+	// metodo para redirigir a la vista de compras del usuario
 	@GetMapping("/compras")
 	public String compras(HttpSession session, Model model) {
 		model.addAttribute("sesion", session.getAttribute("idUsuario"));
@@ -84,6 +84,15 @@ public class UsuarioController {
 		model.addAttribute("ordenes", ordenes);
 		return "usuario/compras";
 	}
-	
+
+	@GetMapping("/detalle/{id}")
+	public String detalleCompra(HttpSession session, Model model, @PathVariable Integer id) {
+		// sesion de usuario o idUsuario
+		model.addAttribute("sesion", session.getAttribute("idUsuario"));
+		LOGGER.info("Id de la orden: {}", id);
+		Optional<Orden> orden = ordenService.findById(id);
+		model.addAttribute("detalles", orden.get().getDetalle());
+		return "usuario/detallecompra";
+	}
 
 }
